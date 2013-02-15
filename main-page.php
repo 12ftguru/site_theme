ad75d424b687d941e7e99a1625d09f8c718996ee
Bryan P Johnson <bryan@12ftguru.com>
2013-02-15 14:41:58 -0500

    In response to John's comment about comments...

---
+++ main-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template used for displaying main page content. Called from front-page.php
+ *
+ * @package Twelve Foot Guru
+ * @since Twelve Foot Guru 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twelvefootguru' ), 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
